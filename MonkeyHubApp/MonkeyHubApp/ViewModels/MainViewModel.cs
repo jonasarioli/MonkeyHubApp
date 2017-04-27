9575e1c074a55ef74788dea74160d67c9ad50955
Jonas Arioli <jarioli@lenovo.com>
2017-04-27 11:29:12 -0300

    Add project files.

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _searchTerm;
+
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set { SetProperty(ref _searchTerm, value); }
+        }
+
+        ICommand 
+
+    }
+}
